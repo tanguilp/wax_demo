11127a95a18287dbb7833acc22b153e4d12b05e2
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-09-12 11:23:53 +0300

    Changed wax dep to github project

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule WaxDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:wax, path: "../wax"}
+      {:wax, github: "tanguilp/wax", tag: "0.1.2"}
     ]
   end
 end
