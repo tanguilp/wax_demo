af15caa06874329af635fe369665deee620ba3da
Tangui <29804907+tanguilp@users.noreply.github.com>
2020-09-26 20:44:33 +0300

    Use wax_ 0.4.0

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule WaxDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:wax_, "~> 0.3.0"}
+      {:wax_, "~> 0.4.0"}
     ]
   end
 end
